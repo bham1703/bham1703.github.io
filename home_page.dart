1ee9ff726e335e92286a0d257564a2e8043681df
bham1703 <125074935+bham1703@users.noreply.github.com>
2023-04-11 22:18:10 +0400

    Add files via upload

--- home_page.dart
+++ home_page.dart
@@ -8,6 +8,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int currentPage = 0;
+
   @override
   Widget build(BuildContext context) {
     //need to add logout button too
@@ -15,6 +17,24 @@ class _HomePageState extends State<HomePage> {
       //removed const here as appbar can be changed
       appBar: AppBar(
         title: const Text('Health System App'), //no name for app yet
+      ),
+
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+          NavigationDestination(icon: Icon(Icons.chat), label: 'Messages'),
+          NavigationDestination(
+              icon: Icon(Icons.person_2_sharp), label: 'Find Doctor'),
+          NavigationDestination(
+              icon: Icon(Icons.location_city), label: 'Location'),
+          NavigationDestination(icon: Icon(Icons.person_2), label: 'Profile'),
+        ],
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPage = index;
+          });
+        },
+        selectedIndex: currentPage,
       ),
     );
   }
